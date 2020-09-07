86391cac9ec54e0d24c7db06a7d16f9a971de2e3
ChrisAchinga <achinga.chris@gmail.com>
2020-09-07 23:39:15 +0300

    home page clean

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "The Boring Blog"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -15,21 +15,20 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+        {/* <img style={{ margin: 0 }} src="./default.png" alt="Gatsby Scene" /> */}
         <h1>
           Hey people{" "}
           <span role="img" aria-label="wave emoji">
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
+        <p>Welcome to my Blogging site. Do not expect so much fun stuff, because it's too technical</p>
         <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
+          As much as I am also a front end developer, this site may or may not look that classy, but it works!!. Will probably make it cool overtime, but I constantly update my stuffs.
         </p>
-        <p>Now go build something great!</p>
+        <p>Now go read some stuffs</p>
         <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
+          <Button marginTop="35px">Articles</Button>
         </Link>
       </Layout>
     )
