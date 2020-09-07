a38ecc16e44692bd905a5ca6e7ea16732f6f32f4
ChrisAchinga <achinga.chris@gmail.com>
2020-09-07 23:34:57 +0300

    edited

--- src/components/bio.js
+++ src/components/bio.js
@@ -34,8 +34,7 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong>, a framework built upon the
-              React library.
+              Written by <strong>{author}</strong>, another developer's blog
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
                 Follow me on Twitter
@@ -50,7 +49,7 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/gatsby-icon.png/" }) {
+    avatar: file(absolutePath: { regex: "/default.png/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
           ...GatsbyImageSharpFixed
