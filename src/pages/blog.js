6fdb8c27badad2d5b37b5969a19181ea0cb2b282
ChrisAchinga <achinga.chris@gmail.com>
2020-09-07 23:39:03 +0300

    blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -16,7 +16,7 @@ class Blog extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
+        <SEO title="All Articles" />
         <Bio />
         <SearchPosts
           posts={posts}
@@ -25,7 +25,7 @@ class Blog extends React.Component {
           location={location}
         />
         <Link to="/">
-          <Button marginTop="85px">Go Home</Button>
+          <Button marginTop="85px">Home</Button>
         </Link>
       </Layout>
     )
