d743c4a5ae437c27824b79fc219938d4a8b4c46e
ChrisAchinga <achinga.chris@gmail.com>
2021-06-20 21:36:58 +0300

    products and services pages

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import Layout from '../layout/Layout'
 import Card from '../components/Card'
 
@@ -5,14 +7,45 @@ export default function Home() {
   return (
     <Layout>
       {/* <!-- Header--> */}
-      <header className='bg-dark py-5'>
-        <div className='container px-4 px-lg-5 my-5'>
-          <div className='text-center text-white'>
-            <h1 className='display-4 fw-bolder'>Ecom Mass</h1>
-            <p className='lead fw-normal text-white-50 mb-0'>Shop and Go</p>
+      <div className='container col-xxl-8 px-4 py-5'>
+        <div className='row flex-lg-row-reverse align-items-center g-5 py-5'>
+          <div className='col-10 col-sm-8 col-lg-6'>
+            <Image
+              src='/bootstrap-themes.png'
+              className='d-block mx-lg-auto img-fluid'
+              alt='Bootstrap Themes'
+              width='700'
+              height='500'
+              loading='lazy'
+            />
+          </div>
+          <div className='col-lg-6'>
+            <h1 className='display-5 fw-bold lh-1 mb-3'>
+              Responsive left-aligned hero with image
+            </h1>
+            <p className='lead'>
+              Quickly design and customize responsive mobile-first sites with
+              Bootstrap, the world’s most popular front-end open source toolkit,
+              featuring Sass variables and mixins, responsive grid system,
+              extensive prebuilt components, and powerful JavaScript plugins.
+            </p>
+            <div className='d-grid gap-2 d-md-flex justify-content-md-start'>
+              <button
+                type='button'
+                className='btn btn-primary btn-lg px-4 me-md-2'
+              >
+                Primary
+              </button>
+              <button
+                type='button'
+                className='btn btn-outline-secondary btn-lg px-4'
+              >
+                Default
+              </button>
+            </div>
           </div>
         </div>
-      </header>
+      </div>
       {/* products */}
       <section className='py-5'>
         <h2 className='text-center'>Products</h2>
@@ -28,6 +61,13 @@ export default function Home() {
             <Card />
             <Card />
           </div>
+        </div>
+        <div className='text-center align-items-center'>
+          <Link href='/products'>
+            <button className='btn btn-outline-primary '>
+              View All Products
+            </button>
+          </Link>
         </div>
       </section>
       {/* Services */}
@@ -46,6 +86,14 @@ export default function Home() {
             <Card />
             <Card />
           </div>
+        </div>
+        <div className='text-center align-items-center'>
+          <Link href='/services'>
+
+          <button className='btn btn-outline-primary '>
+            View All Services
+          </button>
+          </Link>
         </div>
       </section>
     </Layout>
---
+++ pages/products/index.js
@@ -0,0 +1,40 @@
+import Layout from '../../layout/Layout'
+import Card from '../../components/Card'
+
+export default function Home() {
+  return (
+    <Layout>
+      {/* <!-- Header--> */}
+      <header className='bg-dark py-5'>
+        <div className='container px-4 px-lg-5 my-5'>
+          <div className='text-center text-white'>
+            <h1 className='display-4 fw-bolder'>Prodcuts</h1>
+            <p className='lead fw-normal text-white-50 mb-0'>Shop and Go</p>
+          </div>
+        </div>
+      </header>
+      {/* products list */}
+      <section className='py-5'>
+        <h2 className='text-center'>Products</h2>
+        <div className='container px-4 px-lg-5 mt-5'>
+          <div className='row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center'>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </div>
+        <div className='text-center align-items-center'>
+          <button className='btn btn-outline-primary '>
+            Next Page
+          </button>
+        </div>
+      </section>
+    </Layout>
+  )
+}
