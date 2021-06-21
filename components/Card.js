511453073afb25cc62f82c8c472791569d22d7c9
ChrisAchinga <achinga.chris@gmail.com>
2021-06-21 20:49:27 +0300

    get products

--- components/Card.js
+++ components/Card.js
@@ -2,17 +2,24 @@ import Image from 'next/image'
 
 const Card = ({ name, price, image }) => {
   return (
-    <div className='col mb-5'>
-      <div className='card'>
-        <Image src='/pop.png' className='card-img-top' alt='...' layout='fill' />
-        <div className='card-body'>
-          <h5 className='card-title'>Card title</h5>
-          <p className='card-text'>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+    <div class='col-lg-6 mb-4'>
+      <div class='card h-100'>
+        <a href='#'>
+          <Image
+            class='card-img-top'
+            src={image}
+            width={700}
+            height={400}
+            alt=''
+          />
+        </a>
+        <div class='card-body'>
+          <h4 class='card-title'>
+            <a href='#'>{name}</a>
+          </h4>
+          <p class='card-text'>USD. {price}</p>
           <a href='#' className='btn btn-primary'>
-            Go somewhere
+            View
           </a>
         </div>
       </div>
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,7 +1,8 @@
 import Layout from '../../layout/Layout'
 import Card from '../../components/Card'
 
-export default function Home() {
+export default function Home({ products }) {
+  console.log('PRODUCTS', products)
   return (
     <Layout>
       {/* <!-- Header--> */}
@@ -16,25 +17,29 @@ export default function Home() {
       {/* products list */}
       <section className='py-5'>
         <h2 className='text-center'>Products</h2>
-        <div className='container px-4 px-lg-5 mt-5'>
+        <div className='container mt-5'>
           <div className='row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center'>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {products.map((product) => (
+              <Card key={product.id} name={product.name} image={product.cover_image.formats.medium.url} price={product.price} />
+            ))}
           </div>
         </div>
         <div className='text-center align-items-center'>
-          <button className='btn btn-outline-primary '>
-            Next Page
-          </button>
+          <button className='btn btn-outline-primary '>View</button>
         </div>
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps(context) {
+  // get all products
+  const res = await fetch('https://ecom-cms-strapi.herokuapp.com/products')
+  const products = await res.json()
+
+  return {
+    props: {
+      products,
+    },
+  }
 }
