807b3a17c3106ddf760a5c76bd741f6df794f1bc
ChrisAchinga <achinga.chris@gmail.com>
2021-06-21 21:15:03 +0300

    fixing deployment

--- components/ServiceItem.js
+++ components/ServiceItem.js
@@ -1,21 +1,21 @@
 const ServiceItem = ({ name, description }) => {
   return (
-    <div class='row mt-4 mb-3'>
-      <div class='col-md-7'>
+    <div className='row mt-4 mb-3'>
+      <div className='col-md-7'>
         <a href='#'>
           <img
-            class='img-fluid rounded mb-3 mb-md-0'
+            className='img-fluid rounded mb-3 mb-md-0'
             src='http://placehold.it/700x300'
             alt=''
           />
         </a>
       </div>
-      <div class='col-md-5'>
+      <div className='col-md-5'>
         <h3>{name}</h3>
         <p>
           {description}
         </p>
-        <a class='btn btn-primary' href='#'>
+        <a className='btn btn-primary' href='#'>
           View service
         </a>
       </div>
